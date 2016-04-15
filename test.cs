5d7003a141ff15c1fd5c3ec569bfaefacb92c767
wuyanxian <826593532@qq.com>
2016-04-16 02:58:07 +0800

    add Test_Tom

--- test.cs
+++ test.cs
@@ -26,7 +26,15 @@ namespace hebinlieyin_unit_test
             StringWriter output = new StringWriter();
             hebinlieyin_unit_test.Program.HB(input, input2, output); 
             Assert.That(output.ToString(), Is.EqualTo("jack先生(身份证字号123458)为本校专任教师，聘期4年。\r\n\r\n\r\n")); 
-        } 
+        }
 
+        public void Test_Tom()
+        {
+            StringReader input = new StringReader("Tom\t12335678\t1年");
+            StringReader input2 = new StringReader("${中文姓名}先生(身份证字号${身份证字号})为本校专任教师，聘期${年数}。");
+            StringWriter output = new StringWriter();
+            hebinlieyin_unit_test.Program.HB(input, input2, output);
+            Assert.That(output.ToString(), Is.EqualTo("Tom先生(身份证字号12335678)为本校专任教师，聘期1年。\r\n\r\n\r\n"));
+        }
     }
 }
