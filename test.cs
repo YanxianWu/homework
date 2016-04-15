0ddc978726cb6a226d8cfc24cc7b2324589a84bb
wuyanxian <826593532@qq.com>
2016-04-16 02:54:27 +0800

    add Test_jack

--- test.cs
+++ test.cs
@@ -19,5 +19,14 @@ namespace hebinlieyin_unit_test
             Assert.That(output.ToString(), Is.EqualTo("Tomc先生(身份证字号12345678)为本校专任教师，聘期2年。\r\n\r\n\r\n")); 
         } 
 
+        public void Test_jack() 
+        { 
+            StringReader input = new StringReader("jack\t123458\t4年"); 
+            StringReader input2 = new StringReader("${中文姓名}先生(身份证字号${身份证字号})为本校专任教师，聘期${年数}。"); 
+            StringWriter output = new StringWriter();
+            hebinlieyin_unit_test.Program.HB(input, input2, output); 
+            Assert.That(output.ToString(), Is.EqualTo("jack先生(身份证字号123458)为本校专任教师，聘期4年。\r\n\r\n\r\n")); 
+        } 
+
     }
 }
